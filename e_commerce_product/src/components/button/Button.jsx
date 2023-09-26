84eabea4da53ef350c907a91d6a6b7a5786b96b1
MJ <80272444+minai621@users.noreply.github.com>
2023-09-26 10:56:17 +0000

    feat: add Button component

--- e_commerce_product/src/components/button/Button.jsx
+++ e_commerce_product/src/components/button/Button.jsx
@@ -1,5 +1,33 @@
-const Button = () => {
-  return <div>Button</div>;
+import classNames from "classnames";
+import styles from "./Button.module.scss";
+
+const Button = ({
+  type = "button",
+  secondary = false,
+  bgColor,
+  fgColor,
+  width,
+  ...restProps
+}) => {
+  const composeClasses = classNames(
+    styles.button,
+    secondary ? styles.secondary : styles.primary
+  );
+
+  const style = {
+    backgroundColor: bgColor || "",
+    color: fgColor || "",
+    width: width || "",
+  };
+
+  return (
+    <button
+      type={type}
+      style={style}
+      className={composeClasses}
+      {...restProps}
+    />
+  );
 };
 
 export default Button;
