133fc617e9efccc4f0a53eb6b192404b65ef55b9
MJ <80272444+minai621@users.noreply.github.com>
2023-09-26 10:56:37 +0000

    feat: add Loader component

--- e_commerce_product/src/components/loader/Loader.jsx
+++ e_commerce_product/src/components/loader/Loader.jsx
@@ -1,5 +1,33 @@
-const Loader = () => {
-  return <div>Loader</div>;
+import { RotatingLines } from "react-loader-spinner";
+import styles from "./Loader.module.scss";
+
+const Loader = ({ basic }) => {
+  if (basic) {
+    return (
+      <div className={styles.basicWrapper}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible
+        />
+      </div>
+    );
+  }
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}>
+        <RotatingLines
+          strokeColor="grey"
+          strokeWidth="5"
+          animationDuration="0.75"
+          width="30"
+          visible
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
