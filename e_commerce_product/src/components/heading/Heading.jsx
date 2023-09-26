0786201910535c1bc3a00dda829428d6a6e587a7
MJ <80272444+minai621@users.noreply.github.com>
2023-09-26 11:31:30 +0000

    feat: add Heading component

--- e_commerce_product/src/components/heading/Heading.jsx
+++ e_commerce_product/src/components/heading/Heading.jsx
@@ -1,5 +1,13 @@
-const Heading = () => {
-  return <div>Heading</div>;
+import styles from "./Heading.module.scss";
+const Heading = ({ title, subTitle, center }) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={center ? styles.center : ""}>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Heading;
