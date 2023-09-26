c15780ee35137bf95b60c9bd230d937c0815c7e9
MJ <80272444+minai621@users.noreply.github.com>
2023-09-26 10:55:07 +0000

    feat: add Divider  component

--- e_commerce_product/src/components/divider/Divider.jsx
+++ e_commerce_product/src/components/divider/Divider.jsx
@@ -1,5 +1,25 @@
-const Divider = () => {
-  return <div>Divider</div>;
+import styles from './Divider.module.scss';
+import classNames from "classnames";
+
+const Divider = ({
+  space = 22,
+  color = "#ccc",
+  className = "",
+  ...restProps
+}) => {
+  const style = {
+    marginTop: space,
+    marginBottom: space,
+    background: color,
+  };
+  return (
+    <div
+      role="presentation"
+      className={classNames(styles.line, className)}
+      style={style}
+      {...restProps}
+    />
+  );
 };
 
 export default Divider;
