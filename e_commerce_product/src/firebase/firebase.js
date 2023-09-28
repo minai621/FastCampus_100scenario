e157191e740d4afa8b42c7c9ad8d46df3d83f632
MJ <80272444+minai621@users.noreply.github.com>
2023-09-28 07:10:57 +0000

    fix: correcting typos

--- e_commerce_product/src/firebase/firebase.js
+++ e_commerce_product/src/firebase/firebase.js
@@ -1,8 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firesotre";
-import { getStorage } from "firebase/storage";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage"; // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
