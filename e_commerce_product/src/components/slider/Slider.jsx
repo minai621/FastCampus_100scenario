5c5840ca7d15d3e0ff0340a45684caaf6466b227
MJ <80272444+minai621@users.noreply.github.com>
2023-09-30 06:08:42 +0000

    feat: implement Slider

--- e_commerce_product/src/components/slider/Slider.jsx
+++ e_commerce_product/src/components/slider/Slider.jsx
@@ -1,5 +1,65 @@
+"use client";
+import Image from "next/image";
+import { useCallback, useEffect, useState } from "react";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
+import styles from "./Slider.module.scss";
+import sliderData from "./SliderData";
+
 const Slider = () => {
-  return <div>Slider</div>;
+  const [isLeft, setIsLeft] = useState(false);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const sliderLength = sliderData.length;
+
+  const intervalTime = 3000;
+
+  const nextSlide = useCallback(() => {
+    setIsLeft(false);
+    setCurrentSlide(currentSlide === sliderLength - 1 ? 0 : currentSlide + 1);
+  }, [currentSlide, sliderLength]);
+
+  const prevSlide = useCallback(() => {
+    setIsLeft(true);
+    setCurrentSlide(currentSlide === 0 ? sliderLength - 1 : currentSlide - 1);
+  }, [currentSlide, sliderLength]);
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, intervalTime);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [nextSlide]);
+
+  return (
+    <div className={styles.slider}>
+      <AiOutlineArrowLeft
+        className={`${styles.arrow} ${styles.prev}`}
+        onClick={prevSlide}
+      />
+      <AiOutlineArrowRight
+        className={`${styles.arrow} ${styles.next}`}
+        onClick={nextSlide}
+      />
+
+      {sliderData.map((slider, index) => {
+        const { image, heading } = slider;
+        return (
+          <div
+            key={heading}
+            className={`
+            ${styles.slide}
+            ${index === currentSlide && styles.current}
+            ${isLeft ? styles.slideLeft : styles.slideRight}
+           `}
+          >
+            {index === currentSlide ? (
+              <Image src={image} alt={heading} fill />
+            ) : null}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Slider;
---
+++ e_commerce_product/src/components/slider/SliderData.js
@@ -0,0 +1,22 @@
+const sliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1682686581854-5e71f58e7e3f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    heading: "hiking",
+    desc: "Up to 30% off on all onsale products",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1695527081793-91a2d4b5b103?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    heading: "shelf",
+    desc: "Up to 30% off on all onsale products",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1695711453545-0b33b06df225?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80",
+    heading: "welsh corgi",
+    desc: "Up to 30% off on all onsale products",
+  },
+];
+
+export default sliderData;
