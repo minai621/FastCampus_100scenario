52846697935141696eeeb892e25fe1bbd1426f89
MJ <80272444+minai621@users.noreply.github.com>
2023-09-30 14:43:53 +0000

    chore: add "images.unsplash.com" as the image domains in nextConfig

--- e_commerce_product/next.config.js
+++ e_commerce_product/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { swcMinify: true };
+const nextConfig = {
+  swcMinify: true,
+  images: {
+    domains: ["images.unsplash.com"],
+  },
+};
 
 module.exports = nextConfig;
