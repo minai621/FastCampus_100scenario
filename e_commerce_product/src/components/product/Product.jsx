179f674f14d70699a67de2026abf0d7514aefc11
MJ <80272444+minai621@users.noreply.github.com>
2023-10-01 01:06:50 +0000

    feat: implement Product

--- e_commerce_product/src/components/product/Product.jsx
+++ e_commerce_product/src/components/product/Product.jsx
@@ -1,5 +1,34 @@
+"use client";
+
+import useFetchCollection from "@/hooks/useFetchCollection";
+import { GET_PRICE_RANGE, STORE_PRODUCTS } from "@/redux/slice/productSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Loader from "../loader/Loader";
+import styles from "./Product.module.scss";
+import ProductFilter from "./productFilter/ProductFilter";
+import ProductList from "./productList/ProductList";
+
 const Product = () => {
-  return <div>Product</div>;
+  const { data, isLoading } = useFetchCollection("products");
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(STORE_PRODUCTS({ products: data }));
+    dispatch(GET_PRICE_RANGE({ products: data }));
+  }, [data, dispatch]);
+
+  return (
+    <section className={styles.product}>
+      <aside className={styles.filter}>
+        {isLoading ? null : <ProductFilter />}
+      </aside>
+      <div className={styles.content}>
+        {isLoading ? <Loader basic /> : <ProductList />}
+      </div>
+    </section>
+  );
 };
 
 export default Product;
