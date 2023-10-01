cfa43bfc89cd46478b24dc4409a3ec89d2293503
MJ <80272444+minai621@users.noreply.github.com>
2023-10-01 01:06:09 +0000

    feat: useFetchDocuments custom hook

---
+++ e_commerce_product/src/hooks/useFetchDocuments.js
@@ -0,0 +1,31 @@
+import { db } from "@/firebase/firebase";
+import { collection, getDocs, where } from "firebase/firestore";
+import { useCallback, useEffect, useState } from "react";
+
+const useFetchDocuments = (collectionName, arg) => {
+  const [documents, setDocuments] = useState([]);
+
+  const getDocuments = useCallback(async () => {
+    const q = query(
+      collection(db, collectionName),
+      where(arg[0], arg[1], arg[2])
+    );
+    const querySnapshot = await getDocs(q);
+
+    let documentArray = [];
+
+    querySnapshot.forEach((doc) => {
+      documentArray.push(doc.data());
+    });
+
+    setDocuments(documentArray);
+  }, [collectionName, arg[0], arg[1], arg[2]]);
+
+  useEffect(() => {
+    getDocuments();
+  }, [getDocuments]);
+
+  return { documents };
+};
+
+export default useFetchDocuments;
