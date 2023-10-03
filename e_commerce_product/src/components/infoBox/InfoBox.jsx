5ef423d77c1e450f67060174eafb0352e74b2544
MJ <80272444+minai621@users.noreply.github.com>
2023-10-03 14:37:44 +0000

    feat: implement InfoBox

--- e_commerce_product/src/components/infoBox/InfoBox.jsx
+++ e_commerce_product/src/components/infoBox/InfoBox.jsx
@@ -1,5 +1,18 @@
-const InfoBox = () => {
-  return <div>InfoBox</div>;
+import React from "react";
+import styles from "./InfoBox.module.scss";
+
+const InfoBox = ({ cardClass, title, count, icon }) => {
+  return (
+    <div className={styles.infoBox}>
+      <div className={cardClass}>
+        <h4>{title}</h4>
+        <span>
+          <h3>{count}</h3>
+          {icon}
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default InfoBox;
