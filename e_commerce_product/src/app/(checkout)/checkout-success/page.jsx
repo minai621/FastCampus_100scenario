3bc8df7f3d6a4e16a409e937e33d8db96d1375f3
MJ <80272444+minai621@users.noreply.github.com>
2023-10-03 08:04:18 +0000

    fix: invalid data

--- e_commerce_product/src/app/(checkout)/checkout-success/page.jsx
+++ e_commerce_product/src/app/(checkout)/checkout-success/page.jsx
@@ -1,12 +1,14 @@
+import Button from "@/components/button/Button";
 import Heading from "@/components/heading/Heading";
 import { formatTime } from "@/utils/dayjs";
 import priceFormat from "@/utils/priceFormat";
+import Link from "next/link";
 import styles from "./CheckoutSuccess.module.scss";
-("");
 
 const CheckoutSuccess = async ({ searchParams }) => {
   const secretKey = process.env.NEXT_PUBLIC_TOSS_SECRET;
-  const url = `https://api.tosspayments.com/v1/payments/order/${searchParams.orderId}`;
+
+  const url = `https://api.tosspayments.com/v1/payments/orders/${searchParams.orderId}`;
   const basicToken = Buffer.from(`${secretKey}:`, "utf-8").toString("base64");
 
   const payment = await fetch(url, {
@@ -14,9 +16,16 @@ const CheckoutSuccess = async ({ searchParams }) => {
       Authorization: `Basic ${basicToken}`,
       "Content-Type": "application/json",
     },
-  }).then((res) => res.json());
+  }).then((res) => {
+    return res.json();
+  });
 
+  console.log("payment", payment);
   const { card } = payment;
+
+  if (payment.failure) {
+    return <div>다시 시도해주세요.</div>;
+  }
 
   return (
     <section className={styles.success}>
@@ -36,15 +45,15 @@ const CheckoutSuccess = async ({ searchParams }) => {
         </li>
         <li>
           <b>결제 금액:</b>
-          {priceFormat(card.amount)}
+          {priceFormat(card.amount)}원
         </li>
         <li>
-          <b>결제 승인 날짜: </b>
-          {formatTime(payment.approveAt)}
+          <b>결제승인날짜:</b>
+          {formatTime(payment.approvedAt)}
         </li>
       </ul>
       <Button>
-        <Link href="/order-history">주문 정보 보기</Link>
+        <Link href="/order-history">주문 상태 보기</Link>
       </Button>
     </section>
   );
